ece0def8614235bcd58cb0faae106365587c969c
makehoney <makehoney3@gmail.com>
2019-03-26 02:26:51 +0900

    Navbar in progress...

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,115 +1,147 @@
 <template>
-  <v-app dark>
+  <div id="app">
+    <v-toolbar
+      app
+      flat
+    >
+      <v-toolbar-side-icon
+        class="hidden-md-and-up"
+        @click="drawer = !drawer"
+      />
+
+      <v-container mx-auto py-0>
+        <v-layout>
+
+          <v-img
+            :src="require('@/assets/logo.png')"
+            style="cursor: pointer;"
+            class="mr-3"
+            contain
+            height="48"
+            width="48"
+            max-width="48"
+            @click="$router.push('/')"
+          />
+
+
+          <v-toolbar-title
+            style="cursor: pointer;"
+            class="mt-3 ml-0 mr-5"
+            @click="$router.push('/')">AJOU COIN</v-toolbar-title>
+          <v-btn
+            v-for="(link, i) in links"
+            :key="i"
+            :to="link.to"
+            class="hidden-sm-and-down"
+            flat
+          >
+            {{ link.text }}
+          </v-btn>
+          <v-menu
+            transition="slide-x-transition"
+            bottom
+            right
+          >
+            <template
+              #activator="{ on }"
+              v-for="(link, i) in links">
+              <v-btn
+                :key="i"
+                :to="link.to"
+                class="hidden-sm-and-down"
+                flat
+                v-on="on"
+              >
+                {{ link.text }}
+              </v-btn>
+            </template>
+
+            <v-list>
+              <v-list-tile
+                v-for="(item, i) in items"
+                :key="i"
+                @click=""
+              >
+                <v-list-tile-title>{{ item.text }}</v-list-tile-title>
+              </v-list-tile>
+            </v-list>
+          </v-menu>
+
+
+
+          <v-spacer />
+
+        </v-layout>
+      </v-container>
+    </v-toolbar>
+
     <v-navigation-drawer
       v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
+      absolute
+      temporary
     >
-      <v-list>
-        <v-list-tile
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-tile-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-tile-action>
+      <v-list class="pa-1">
+        <v-list-tile avatar>
+          <v-list-tile-avatar>
+            <img src="/home/brian/Desktop/capstone/ajou-coin/client/assets/logo.png">
+          </v-list-tile-avatar>
+
           <v-list-tile-content>
-            <v-list-tile-title v-text="item.title" />
+            <v-list-tile-title>John Leider</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+
+      <v-list class="pt-0" dense>
+        <v-divider></v-divider>
+
+        <v-list-tile
+          v-for="item in links"
+          :key="item.text"
+          @click=""
+        >
+
+          <v-list-tile-content>
+            <v-list-tile-title>{{ item.text }}</v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
-    <v-toolbar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
-      <v-toolbar-side-icon @click="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>{{ `chevron_${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>remove</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>menu</v-icon>
-      </v-btn>
-    </v-toolbar>
-    <v-content>
-      <v-container>
-        <nuxt />
-      </v-container>
-    </v-content>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-tile @click.native="right = !right">
-          <v-list-tile-action>
-            <v-icon light>compare_arrows</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer
-      :fixed="fixed"
-      app
-    >
-      <span>&copy; 2019</span>
-    </v-footer>
-  </v-app>
+  </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'bubble_chart',
-          title: 'Inspire',
-          to: '/inspire'
-        }
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
-    }
-  }
+  data: () => ({
+    links: [
+      {
+        text: '자유게시판',
+        to: 'https://naver.com'
+      },
+      {
+        text: '취업게시판',
+        to: 'https://naver.com'
+      },
+      {
+        text: '취업게시판',
+        to: 'https://naver.com'
+      },
+      {
+        text: '취업게시판',
+        to: 'https://naver.com'
+      }
+    ],
+    drawer: false,
+    items: [
+      {
+        text: 'Home',
+        to: '/'
+      },
+      {
+        text: 'About',
+        href: '#about'
+      }
+    ]
+  })
 }
 </script>
