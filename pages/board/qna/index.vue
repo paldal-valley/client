03f1d472070e094fd128d3cdd6b4d233fdd56c9e
effya <shs97110@gmail.com>
2019-04-29 19:42:02 +0900

    add q&A board front template

--- pages/board/qna/index.vue
+++ pages/board/qna/index.vue
@@ -1,21 +1,39 @@
 <template>
-  <div class="text-xs-center">
-    <v-pagination v-model="page" :length="6"></v-pagination>
+  <div>
+    <h1>Q&A</h1>
+    <paginated-list :list-array="pageArray" />
   </div>
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        page: 1
-      }
+import axios from 'axios'
+import PaginatedList from './PaginatedList'
+export default {
+  name: 'simple-pagination',
+  components: {
+    PaginatedList
+  },
+  data () {
+    return {
+      pageArray: []
     }
+  },
+  created () {
+    axios.get('http://sample.bmaster.kro.kr/contacts')
+    .then(response => {
+      console.log(response);
+      this.pageArray = response.data.contacts;
+    })
+    .catch(err => {
+      console.log(err);
+    });
   }
+}
 </script>
 
-<style scoped>
-.next {
-  color: White;
+<style>
+h1 {
+  color: #454545;
+  text-align: center;
 }
 </style>
