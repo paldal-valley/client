d9dde45578551f534abd5fa14e9ab3f75fbc577a
JeongDawon <fors8@ajou.ac.kr>
2019-05-02 20:20:38 +0900

    newpost.vue template slightly modified

--- pages/board/qna/post.vue
+++ pages/board/qna/post.vue
@@ -3,8 +3,7 @@
     <v-form>
       <v-divider></v-divider>
       <v-text-field
-        label="Subject"
-        value="Plans for the weekend"
+        label="제목"
         single-line
         full-width
         hide-details
@@ -12,9 +11,8 @@
       <v-divider></v-divider>
       <v-textarea
         v-model="title"
-        label="Message"
+        label="내용"
         counter
-        maxlength="120"
         full-width
         single-line
       ></v-textarea>
@@ -26,9 +24,7 @@
   export default {
     data () {
       return {
-        selected: ['Trevor Handsen'],
-        items: ['Trevor Handsen', 'Alex Nelson'],
-        title: 'Hi,\nI just wanted to check in and see if you had any plans the upcoming weekend. We are thinking of heading up to Napa'
+        
       }
     }
   }
