cfde894ccb9bd1890e9aaed8d9b780c456de41c2
makehoney <makehoney3@gmail.com>
2019-05-09 15:57:48 +0900

    [Add feature] 랜딩 페이지 게시판 카드컴포넌트 추가

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,36 @@
 <template>
-  <h1>Test Landing Page</h1>
+  <div>
+    <vue-board-card-group
+      :board-cards="boardCards"/>
+  </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
+import VueBoardCardGroup from '~/components/common/cards/board/group'
 
 export default {
   components: {
-    Logo,
-    VuetifyLogo
-  }
+    VueBoardCardGroup
+  },
+  data: () => ({
+    boardCards: [
+      {
+        title: '자유게시판',
+        content: '선후배간 자유롭게 대화하세요!',
+        imgSrc: require('@/assets/images/free.png')
+      },
+      {
+        title: 'QnA 게시판',
+        content: '질문과 답변을 올려보세요!',
+        imgSrc: require('@/assets/images/qna.png')
+      },
+      {
+        title: '수기게시판',
+        content: '취업한 선배들의 꿀팁!',
+        imgSrc: require('@/assets/images/consult.png')
+      }
+    ]
+  })
 }
 </script>
+
