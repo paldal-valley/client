36f1dbd9939f65d93e0cd91bf0aa4d2a689fb884
JeongDawon <fors8@ajou.ac.kr>
2019-05-19 02:47:53 +0900

    답변쓰기 front 완성

--- pages/board/qna/_id.vue
+++ pages/board/qna/_id.vue
@@ -1,14 +1,50 @@
 <template>
+<div>
+  <vue-post
+    :title="posts.title"
+    :content="posts.content"
+    :createdDate="posts.createdDate"
+    :view="posts.view"
+    :hasAnswerBtn = false
+    :questionId="posts.id"
+    ></vue-post>
 
-<h1> Hello {{$route.params.id}} </h1>
-
+  <vue-answer
+    :hasCategory="false"
+    :hasTitle="false"
+  ></vue-answer>
+</div>
 </template>
 
+
+
 <script>
+import VuePost from "~/components/common/posts/index" ;
+import VueAnswer from "~/components/common/posts/write" ;
+export default {
+  components: {
+  VuePost, VueAnswer
+},
+data(){
+     return {
+       posts:
+       {
+         title: '', //제목
+         content: '', //내용
+         writer: '', // userId
+         view: '', // 조회수
+         recommended: '', // 추천수
+         createdDate: '', //작성일
+         lastModifiedDate: '', //최종 수정일
+         isPending: '',
+         isDeleted: '',
+         type: ''
+       }
+     }
+   },
 
+}
 </script>
-
-<style scoped>
-
+<style>
 
 </style>
