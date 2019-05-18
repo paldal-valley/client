7c1b7af2bc702edf911e3b0c8a3b013a851822c6
makehoney <makehoney3@gmail.com>
2019-05-18 22:05:19 +0900

    [Add pages] 서브 게시판 페이지 추가

--- pages/board/plaza/_postId/index.vue
+++ pages/board/plaza/_postId/index.vue
@@ -1,13 +1,28 @@
 <template>
-    $END$
+  <div>
+    <vue-post
+      title="타이틀"
+      content="내용"
+      category="카테고리"
+      user-name="이병헌"
+      created-date="2019. 01. 01"
+      view="34"/>
+  </div>
 </template>
 
 <script>
+import VuePost from '~/components/common/posts'
 export default {
-  name: 'index'
+  components: {
+    VuePost
+  },
+  methods: {
+    async fetchPost() {
+
+    }
+  }
 }
 </script>
 
 <style scoped>
-
 </style>
