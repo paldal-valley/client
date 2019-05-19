ea6def44a518a2577e24ab68ca1cbbb6db73bb61
makehoney <makehoney3@gmail.com>
2019-05-20 00:34:14 +0900

    [Edit component] 카테고리 태그 추가

--- components/common/cards/post/index.vue
+++ components/common/cards/post/index.vue
@@ -3,10 +3,14 @@
     :to="to"
     class="card-link">
     <div class="card-container">
-      <p v-if="boardTitle">{{ category }}</p>
+      <div
+        v-if="boardTitle"
+        class="card__category">
+        {{ category }}
+      </div>
       <h2 class="card__title">{{ post.title }}</h2>
       <p class="card__content">{{ post.content }}</p>
-      <div> {{ post.createdDate }} </div>
+      <div class="card__date"> {{ post.createdDate }} </div>
     </div>
   </nuxt-link>
 </template>
@@ -77,5 +81,17 @@ export default {
     right: -10px;
     bottom: -10px;
     width: 100px;
+  }
+  .card__category {
+    display: inline-block;
+    box-shadow: 2px 2px 0 0 rgba(0, 0, 0, 0.2);
+    background-color: #4e98a424;
+    color: black;
+    padding: 3px;
+    margin-bottom: 10px;
+    border-radius: 3px;
+  }
+  .card__date {
+    text-align: right;
   }
 </style>
