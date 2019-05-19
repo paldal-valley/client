714abce68579f04529d2d6284a0f82c80571aabd
makehoney <makehoney3@gmail.com>
2019-05-19 21:09:18 +0900

    [Edit pages] vue-board-post-list 컴포넌트 디렉티브 추가

--- pages/board/plaza/food/index.vue
+++ pages/board/plaza/food/index.vue
@@ -7,6 +7,7 @@
     <!-- posts -->
     <vue-board-post-list
       api-endpoint="posts/plaza?category=4"
+      post-link-prefix="/board/plaza/"
       alert-msg="플라자 맛집 페이지입니다."/>
 
   </vue-board-container>
--- pages/board/plaza/job/index.vue
+++ pages/board/plaza/job/index.vue
@@ -7,6 +7,7 @@
     <!-- posts -->
     <vue-board-post-list
       api-endpoint="posts/plaza?category=3"
+      post-link-prefix="/board/plaza/"
       alert-msg="플라자 취업 페이지입니다."/>
 
   </vue-board-container>
