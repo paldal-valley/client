dbcce2ea7fa89cc4ff9ce784fc181a7b57d018ff
makehoney <makehoney3@gmail.com>
2019-05-22 16:12:06 +0900

    [Add plugins] axios 커스톰 플러그인 추가

---
+++ plugins/axios.js
@@ -0,0 +1,16 @@
+import cookie from 'js-cookie'
+
+export default function ({ $axios, store }) {
+  $axios.onRequest(config => {
+
+    // 서버사이드에서 헤더를 설정해준 경우 (nuxtServerInit) 헤더가 사전에 존재할 수 있음
+    if (typeof config.headers.common['x-access-token'] !== 'undefined') return
+
+    if (cookie.get('x-access-token')) {
+      config.headers.common['x-access-token'] = cookie.get('x-access-token')
+    } else {
+      delete config.headers.common['x-access-token']
+      store.commit('auth/RESET_USER')
+    }
+  })
+}
