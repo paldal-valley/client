21d73aee011537ad40b05a8b0553f976766e59e0
makehoney <makehoney3@gmail.com>
2019-05-24 16:32:00 +0900

    [Add pages] 게시판 별 전체글 페이지 생성

--- pages/post/plaza/index.vue
+++ pages/post/plaza/index.vue
@@ -1,24 +1,21 @@
 <template>
-  <div>
-    <vue-board-container>
-      <!-- sidebar -->
-      <vue-board-sidebar
-        :buttons="GET_REVIEW_META.sidebarButtons"
-        :buttons-downside="GET_POST_META.sidebarButtonsDownside"/>
+  <vue-board-container>
+    <!-- sidebar -->
+    <vue-board-sidebar
+      :buttons="GET_PLAZA_META.sidebarButtons"
+      :buttons-downside="GET_POST_META.sidebarButtonsDownside"/>
 
-      <!-- posts -->
-      <vue-board-post-list
-        board-title="review"
-        :buttons="GET_POST_META.icon_buttons"
-        :api-endpoint="GET_REVIEW_META.url"
-        :post-link-prefix="GET_REVIEW_META.url"
-        alert-msg="수기 게시판입니다."
-      />
-      <vue-float-btn
-        class="float-add-post-btn"
-        @click="$router.push(`${GET_REVIEW_META.url}/write/`)"/>
-    </vue-board-container>
-  </div>
+    <!-- posts -->
+    <vue-board-post-list
+      board-title="plaza"
+      :buttons="GET_POST_META.icon_buttons"
+      :api-endpoint="GET_PLAZA_META.url"
+      :post-link-prefix="GET_PLAZA_META.url"
+    />
+    <vue-float-btn
+      class="float-add-post-btn"
+      @click="$router.push(`${GET_PLAZA_META.url}/write/`)"/>
+  </vue-board-container>
 </template>
 
 <script>
@@ -31,6 +28,7 @@ import VueBoardPostList from '~/components/each-page/post/post-list'
 import VueFloatBtn from '~/components/common/buttons/float'
 
 import { mapGetters } from 'vuex'
+
 export default {
   components: {
     VueBoardContainer,
@@ -41,9 +39,14 @@ export default {
   computed: {
     ...mapGetters('page-meta', [
       'GET_POST_META',
-      'GET_REVIEW_META'
+      'GET_PLAZA_META'
     ]),
   },
+  methods: {
+    foo() {
+      console.log('clickckckk')
+    }
+  }
 }
 </script>
 
