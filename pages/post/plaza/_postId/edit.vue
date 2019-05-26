d7a8ff12be3ce044294c9eb4249b100c4c5b0e85
makehoney <makehoney3@gmail.com>
2019-05-26 15:34:16 +0900

    [Add feature] 글 수정 및 삭제 기능 추가

--- pages/post/plaza/_postId/edit.vue
+++ pages/post/plaza/_postId/edit.vue
@@ -1,13 +1,111 @@
 <template>
-    $END$
+  <div class="write-container">
+    <v-card>
+      <v-form>
+        <v-select
+          v-model="post.categoryId"
+          :items="selectorItem"
+          item-value="id"
+          item-text="title"
+          label="카테고리"
+          full-width/>
+
+        <v-divider
+          class="separator"/>
+
+        <v-text-field
+          v-model="post.title"
+          label="제목"
+          full-width/>
+
+        <v-divider
+          class="separator"/>
+
+        <v-textarea
+          v-model="post.content"
+          full-width
+          rows="15"/>
+      </v-form>
+    </v-card>
+    <v-btn
+      block
+      color="#4E98A4"
+      class="white--text"
+      @click="updatePost">
+      <strong>수정 완료</strong>
+    </v-btn>
+  </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 export default {
-  name: 'edit'
+  middleware: ['isLoggedIn'],
+  computed: {
+    ...mapGetters({
+      GET_USER: 'auth/GET_USER'
+    }),
+    selectorItem() {
+      return this.GET_CATEGORIES['plaza']
+    }
+  },
+  data: () => ({
+    postId: '',
+    post: {}
+  }),
+  mounted() {
+    this.postId = this.$route.params.postId
+    this.fetchPost()
+  },
+  methods: {
+    async fetchPost() {
+      try {
+        const options = {
+          url: `post/plaza/${this.postId}`,
+          method: 'get'
+        }
+        const { data } = await this.$axios(options)
+
+        if (data.userId !== this.GET_USER.id) {
+          alert('자신의 포스트만 수정 가능합니다.')
+          this.$router.back()
+        } else {
+          this.post = data
+        }
+
+      } catch (err) {
+        console.error(err)
+      }
+    },
+    async updatePost() {
+      const options = {
+        url: `post/plaza/${this.postId}`,
+        method: 'put',
+        params: { categoryId: this.post.categoryId },
+        data: {
+          title: this.post.title,
+          content: this.post.content
+        }
+      }
+
+      try {
+        await this.$axios(options)
+        this.$router.back()
+      } catch (err) {
+        console.error(err)
+        alert('에러가 발생했습니다.')
+      }
+    }
+  },
 }
 </script>
 
 <style scoped>
-
+  .separator {
+    margin-top: -30px;
+    margin-bottom: 10px;
+  }
+  .write-container {
+    margin-bottom: 20px;
+  }
 </style>
