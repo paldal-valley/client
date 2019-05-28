3ed476bb8f6fc14074d1b28216d8c9f45c912347
makehoney <makehoney3@gmail.com>
2019-05-29 02:58:52 +0900

    [Add Component] 코멘트 관련 컴포넌트 추가

--- components/common/cards/comment/index.vue
+++ components/common/cards/comment/index.vue
@@ -1,44 +1,116 @@
 <template>
-  <nuxt-link :to="to" class="card-link">
+  <div class="card-link">
     <div class="card-container">
-      <div
-        v-if="boardTitle"
-        class="card__category">
-        {{ category }}
+      <div class="first-line">
+        <p
+          v-if="isOwner"
+          :style="myCommentStyle"
+          class="card__username"><strong>{{ userInfo }}</strong></p>
+        <p
+          v-else
+          class="card__username"><strong>{{ userInfo }}</strong></p>
+        <div
+          v-if="isOwner"
+          class="btns">
+          <vue-float-btn
+            icon="edit"
+            :small="true"
+            @click="toggleEditor"/>
+          <vue-float-btn
+            icon="remove"
+            color="red"
+            :small="true"
+            @click="deleteComment"/>
+        </div>
       </div>
-      <h2 class="card__title">{{ post.title }}</h2>
-      <p class="card__content">{{ post.content }}</p>
-      <div class="card__date"> {{ post.createdDate }} </div>
+      <p
+        v-if="!isEditing"
+        class="card__content">{{ comment.content }}</p>
+      <div v-else>
+        <v-textarea
+          v-model="newCommentContent"
+          hide-details
+          full-width
+          rows="4"/>
+        <v-btn
+          class="update-btn"
+          block
+          color="#4E98A4"
+          @click="updateComment">수정</v-btn>
+      </div>
+      <p class="card__date"> {{ comment.createdDate }} </p>
     </div>
-  </nuxt-link>
+  </div>
 </template>
 
 <script>
-// props
-// exist: title, view, id, recommended, content
-// need: createdDate, tag, category
+import VueFloatBtn from '~/components/common/buttons/float'
+
+import { mapGetters } from 'vuex'
 export default {
+  components: {
+    VueFloatBtn
+  },
   props: {
-    post: {
-      type: Object,
-      default: () => ({})
-    },
-    to: {
-      type: String,
-      default: '#'
-    },
-    boardTitle: {
-      type: String,
-      default: ''
-    },
     comment: {
       type: Object,
       default: () => ({})
-    }
+    },
+    newCommentContent: ''
   },
   computed: {
-    category() {
-      return this.$categoryMapper(this.boardTitle, this.post.categoryId)
+    ...mapGetters('auth', [
+      'GET_USER'
+    ]),
+    isOwner() {
+      return this.GET_USER.id === this.comment.userId
+    },
+    userInfo() {
+      return this.isOwner
+        ? '내가 작성한 댓글'
+        : `${this.comment.userName} (${this.comment.userEmail})`
+    }
+  },
+  data: () => ({
+    isEditing: false,
+    myCommentStyle: { color: '#4E98A4' }
+  }),
+  methods: {
+    toggleEditor() {
+      this.isEditing = !this.isEditing
+      if (this.isEditing) this.newCommentContent = this.comment.content.slice()
+    },
+    async updateComment() {
+      const options = {
+        url: `comment/${this.comment.id}`,
+        method: 'put',
+        data: { content: this.newCommentContent }
+      }
+      try {
+        await this.$axios(options)
+        this.$notifySuccess('댓글 수정 완료')
+        this.$emit('comments-changed')
+        this.isEditing = false
+      } catch (err) {
+        console.error(err)
+        this.$notifyError('에러가 발생했습니다.')
+      }
+    },
+    async deleteComment() {
+      if (confirm('정말 댓글을 삭제하시겠어요?')) {
+        const options = {
+          url: `comment/${this.comment.id}`,
+          method: 'delete'
+        }
+        try {
+          await this.$axios(options)
+          this.$notifySuccess('댓글 삭제 완료')
+          this.$emit('comments-changed')
+        } catch (err) {
+          console.error('에러가 발생했습니다.')
+          this.$notifyError('에러가 발생했습니다.')
+        }
+      }
     }
   }
 }
@@ -47,60 +119,39 @@ export default {
 <style lang="scss" scoped>
   @import '~assets/scss/index';
   .card-link {
-    /*flex: 0 0 50%;*/
-    height: 100%;
-    margin: 3px;
     display: block;
-    padding: 0.875rem;
     text-align: left;
-    border-radius: 6px;
     overflow: hidden;
-    border: 1px solid transparent;
-    text-decoration: none !important;
-    /*background-color: blue;*/
+    margin-bottom: 20px;
+    border-radius: 6px;
+    box-shadow: 1px 1px 5px gray;
 
-    position: relative;
-    list-style: none;
-    box-shadow: $box-shadow;
-
-    &:hover {
-      .card__title {
-        color: $ac-green2;
-      }
-
-      .card__content {
-        color: $ac-green2;
-      }
+    .card__username {
+      margin-top: 10px;
     }
 
-    .card__title {
-      color: black;
-      margin-bottom: 10px;
+    .card-container {
+      padding: 0 0.875rem;
+      .first-line {
+        display: flex;
+
+        .btns {
+          display: flex;
+          margin-top: 5px;
+          margin-left: auto;
+        }
+      }
+      .update-btn {
+        color: white;
+        font-weight: bold;
+      }
     }
 
     .card__content {
-      color: $ac-green3;
-    }
-
-    .card__img {
-      position: absolute;
-      right: -10px;
-      bottom: -10px;
-      width: 100px;
-    }
-
-    .card__category {
-      display: inline-block;
-      box-shadow: 2px 2px 0 0 rgba(0, 0, 0, 0.2);
-      background-color: #4e98a424;
       color: black;
-      padding: 3px;
-      margin-bottom: 10px;
-      border-radius: 3px;
     }
 
     .card__date {
-      text-align: right;
       font-weight: bold;
       color: gray;
     }
--- components/common/textareas/comment/index.vue
+++ components/common/textareas/comment/index.vue
@@ -2,21 +2,77 @@
   <v-card class="comment-card">
     <v-textarea
       class="comment-textarea"
-      placeholder="댓글을 작성해보세요!"
+      v-model="commentContent"
       hide-details
       full-width
-      rows="5"/>
+      :placeholder="placeholder"
+      :rows="textareaRow"/>
 
     <v-btn
       class="comment-btn"
       color="#4E98A4"
-      block>완료</v-btn>
+      block
+      @click="createComment">{{ buttonText }}</v-btn>
   </v-card>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 export default {
-  name: 'index'
+  props: {
+    placeholder: {
+      type: String,
+      default: '댓글을 작성해보세요!'
+    },
+    textareaRow: {
+      type: String,
+      default: '5'
+    },
+    buttonText: {
+      type: String,
+      default: '댓글 작성'
+    }
+  },
+  computed: {
+    ...mapGetters('auth', ['GET_USER'])
+  },
+  mounted() {
+    this.postId = this.$route.params.postId
+  },
+  data: () => ({
+    postId: '',
+    commentContent: ''
+  }),
+  methods: {
+    async createComment() {
+      if (!this.isValidComment()) {
+        this.$notifyWarning('올바른 댓글을 입력해주세요!')
+        return
+      }
+
+      const options = {
+        url: `post/${this.postId}/comment`,
+        method: 'post',
+        data: {
+          userId: this.GET_USER.id,
+          content: this.commentContent
+        }
+      }
+      try {
+        await this.$axios(options)
+        this.$notifySuccess('댓글 작성 완료')
+        this.$emit('comment-created')
+        this.commentContent = ''
+      } catch (err) {
+        console.error(err)
+        this.$notifyError('에러가 발생했습니다.')
+      }
+    },
+    isValidComment() {
+      const copy = this.commentContent.slice()
+      return copy.trim()
+    }
+  }
 }
 </script>
 
@@ -28,7 +84,7 @@ export default {
   margin-bottom: 20px;
 
   .comment-textarea {
-    margin-top: 5px;
+    margin-top: 10px;
   }
 
   .comment-btn {
