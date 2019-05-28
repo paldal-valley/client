54631efb097fd09e55d3d8cb45c9c7973a7c2627
makehoney <makehoney3@gmail.com>
2019-05-29 02:55:34 +0900

    [Add component] 댓글 컴포넌트 및 세퍼레이터 컴포넌트 추가 + vue-notification 디렉티브 변경

---
+++ components/common/cards/comment/index.vue
@@ -0,0 +1,108 @@
+<template>
+  <nuxt-link :to="to" class="card-link">
+    <div class="card-container">
+      <div
+        v-if="boardTitle"
+        class="card__category">
+        {{ category }}
+      </div>
+      <h2 class="card__title">{{ post.title }}</h2>
+      <p class="card__content">{{ post.content }}</p>
+      <div class="card__date"> {{ post.createdDate }} </div>
+    </div>
+  </nuxt-link>
+</template>
+
+<script>
+// props
+// exist: title, view, id, recommended, content
+// need: createdDate, tag, category
+export default {
+  props: {
+    post: {
+      type: Object,
+      default: () => ({})
+    },
+    to: {
+      type: String,
+      default: '#'
+    },
+    boardTitle: {
+      type: String,
+      default: ''
+    },
+    comment: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+  computed: {
+    category() {
+      return this.$categoryMapper(this.boardTitle, this.post.categoryId)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import '~assets/scss/index';
+  .card-link {
+    /*flex: 0 0 50%;*/
+    height: 100%;
+    margin: 3px;
+    display: block;
+    padding: 0.875rem;
+    text-align: left;
+    border-radius: 6px;
+    overflow: hidden;
+    border: 1px solid transparent;
+    text-decoration: none !important;
+    /*background-color: blue;*/
+
+    position: relative;
+    list-style: none;
+    box-shadow: $box-shadow;
+
+    &:hover {
+      .card__title {
+        color: $ac-green2;
+      }
+
+      .card__content {
+        color: $ac-green2;
+      }
+    }
+
+    .card__title {
+      color: black;
+      margin-bottom: 10px;
+    }
+
+    .card__content {
+      color: $ac-green3;
+    }
+
+    .card__img {
+      position: absolute;
+      right: -10px;
+      bottom: -10px;
+      width: 100px;
+    }
+
+    .card__category {
+      display: inline-block;
+      box-shadow: 2px 2px 0 0 rgba(0, 0, 0, 0.2);
+      background-color: #4e98a424;
+      color: black;
+      padding: 3px;
+      margin-bottom: 10px;
+      border-radius: 3px;
+    }
+
+    .card__date {
+      text-align: right;
+      font-weight: bold;
+      color: gray;
+    }
+  }
+</style>
---
+++ components/common/textareas/comment/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-card class="comment-card">
+    <v-textarea
+      class="comment-textarea"
+      placeholder="댓글을 작성해보세요!"
+      hide-details
+      full-width
+      rows="5"/>
+
+    <v-btn
+      class="comment-btn"
+      color="#4E98A4"
+      block>완료</v-btn>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'index'
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~assets/scss/index';
+.comment-card {
+  border-radius: 5px;
+  background-color: white;
+  margin-bottom: 20px;
+
+  .comment-textarea {
+    margin-top: 5px;
+  }
+
+  .comment-btn {
+    font-weight: bold;
+    margin-bottom: -2px;
+    color: white;
+  }
+}
+
+</style>
