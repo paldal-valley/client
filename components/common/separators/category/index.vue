4f96a3794cfdae9effde7899b540dff7c2465e66
makehoney <makehoney3@gmail.com>
2019-05-29 02:59:59 +0900

    [Add component] 커스텀 세퍼레이터 추가

--- components/common/separators/category/index.vue
+++ components/common/separators/category/index.vue
@@ -1,13 +1,27 @@
 <template>
-
+  <div>
+    <h2 class="category-name">{{ categoryName }}</h2>
+    <v-divider class="separator"/>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'index'
+  props: {
+    categoryName: {
+      type: String,
+      default: ''
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+.category-name {
+  text-align: left;
+  margin-top: 30px;
+}
+.separator {
+  margin-bottom: 10px;
+}
 </style>
