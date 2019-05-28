0590c973200cfb03209a3fec2f9e39a26d1e11e9
makehoney <makehoney3@gmail.com>
2019-05-29 05:08:57 +0900

    [Edit component] post-list에서 alert 삭제

--- components/each-page/post/post-list/index.vue
+++ components/each-page/post/post-list/index.vue
@@ -1,22 +1,5 @@
 <template>
   <vue-post-container>
-    <!-- alert -->
-    <!-- <div class="alert-container">
-      <v-alert
-        class="post-alert"
-        :value="true"
-        color="#FF3700"
-        icon="check_circle"
-        outline
-      >
-        {{ alertMsg }}
-      </v-alert>
-    </div> -->
-
-    <!-- button-group -->
-    <!-- <vue-button-group :buttons="buttons" /> -->
-
-    <!-- posts -->
     <vue-post-card
       v-for="post in posts"
       :board-title="boardTitle"
@@ -65,11 +48,6 @@ export default {
       type: String,
       default: '',
       required: true
-    },
-    alertMsg: {
-      type: String,
-      default:
-        '아주코인 콘텐츠 관리를 준수하지 않는 질문과 답변의 경우 사전 고지 없이 삭제 처리 되며, 신고 누적 시 사용자 계정 비활성화, 토큰 보상 보류 등의 페널티가 있습니다.'
     },
     buttons: {
       type: Array,
