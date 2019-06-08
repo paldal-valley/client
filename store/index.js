38513c4002641da9843db34875345c9686b3cca8
makehoney <makehoney3@gmail.com>
2019-06-09 06:34:44 +0900

    [Remove code] 레거시 코드 제거

--- store/index.js
+++ store/index.js
@@ -3,67 +3,15 @@ import cookie from 'cookie'
 export const strict = false
 
 export const state = () => ({
-  // accessToken: null,
-  // userId: null
 })
 
 export const getters = {
-  // getUserId: state => state.userId
 }
 
 export const mutations = {
-  // LOGIN(state, { accessToken, userId }) {
-  //   state.accessToken = accessToken
-  //   state.userId = userId
-  //   localStorage.accessToken = accessToken
-  //   localStorage.userId = userId
-  // },
-  // LOGOUT(state) {
-  //   state.accessToken = null
-  //   state.userId = null
-  //   delete localStorage.accessToken
-  //   delete localStorage.userId
-  // }
 }
 
 export const actions = {
-  // enhanceAccessToken({ commit }) {
-  //   if (localStorage.accessToken) {
-  //     commit('LOGIN', localStorage)
-  //     this.$axios.defaults.headers.common['Authorization'] = `Bearer ${
-  //       localStorage.accessToken
-  //     }`
-  //   }
-  // },
-  // login_check() {
-  //   // If the user is not authenticated
-  //   if (!localStorage.accessToken) {
-  //     return false
-  //   }
-  //   return true
-  // },
-  // login({ commit }, { userId, password }) {
-  //   return this.$axios
-  //     .$post('/login', {
-  //       // 경로확인
-  //       userId: userId,
-  //       password: password
-  //     })
-  //     .then(res => {
-  //       commit('LOGIN', res)
-  //       this.$axios.defaults.headers.common['Authorization'] = `Bearer ${
-  //         res.accessToken
-  //       }`
-  //     })
-  //     .catch(err => {
-  //       alert(err)
-  //     })
-  // },
-  // logout({ commit }) {
-  //   this.$axios.defaults.headers.common['Authorization'] = undefined
-  //   commit('LOGOUT')
-  //   console.log('logout')
-  // },
   async nuxtServerInit({ dispatch, commit }, ctx) {
     // refresh 하면 store의 user 데이터도 리셋되므로
     // auth/FETCH를 통해서 refresh 시 매번 user데이터를 init 해준다.
