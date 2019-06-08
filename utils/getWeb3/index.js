604e58a6602aa092542dfb7959c5ecb51099146f
makehoney <makehoney3@gmail.com>
2019-06-09 06:36:24 +0900

    [Add utils] web3 얻어오는 유틸리티 추가(레거시 리팩토링)

--- utils/getWeb3/index.js
+++ utils/getWeb3/index.js
@@ -1,68 +1,84 @@
 import Web3 from 'web3'
 
 export const getWeb3 = async () => {
-    let isUserConnected, result
-    const web3 = new Web3(window.web3.currentProvider)
-    const coinbase = (await web3.eth.getAccounts())[0]
-    typeof coinbase === 'undefined'
-    ? isUserConnected = false
-    : isUserConnected = true
+  const web3 = new Web3(window.web3.currentProvider)
+  const coinbase = (await web3.eth.getAccounts())[0]
 
-    if (isUserConnected) {
-        result = { web3 () { return web3 }, coinbase }
+  const isUserConnected = typeof coinbase !== 'undefined'
 
-        let networkID = await result.web3().eth.net.getNetworkType()
-            .catch(err => { throw new Error(`Unable to retrieve network ID, log: ${err}`) })
-        result = Object.assign(result, { networkID })
-
-        let balance = await result.web3().eth.getBalance(result.coinbase)
-            .catch(err => { throw new Error(`Unable to retrieve balance for address: ${result.coinbase}, log: ${err}`) })
-        result = Object.assign(result, { balance })
-
-    } else {
-        throw new Error('Unable to connect to Metamask')
+  if (isUserConnected) {
+    const result = {
+      web3: () => web3,
+      coinbase
     }
+
+    result.networkId = await result.web3().eth.net.getNetworkType()
+      .catch(err => { throw new Error(`Unable to retrieve network ID, log: ${err}`) })
+
+    result.balance = await result.web3().eth.getBalance(result.coinbase)
+      .catch(err => { throw new Error(`Unable to retrieve balance for address: ${result.coinbase}, log: ${err}`) })
+
     return result
+  } else {
+    throw new Error('Unable to connect to Metamask')
+  }
 }
 
 export const pollWeb3 = state => {
-    let web3 = new Web3(window.web3.currentProvider)
+  const isIntervalExist = state.isIntervalExist
+
+  // interval 여러개 실행 방지
+  if (!isIntervalExist) {
+    const web3 = new Web3(window.web3.currentProvider)
 
     setInterval(async () => {
-        if (typeof (await web3.eth.getAccounts())[0] === 'undefined') {
-            clearWeb3Instance(state)
-        } else if (state.web3.web3Instance && ((await web3.eth.getAccounts())[0] !== state.web3.coinbase)) {
-            try {
-                let newCoinbase = (await web3.eth.getAccounts())[0]
-                let newBalnce = await web3.eth.getBalance(newCoinbase)
-                changeCoinbase(state, {
-                    coinbase: newCoinbase,
-                    balance: parseInt(newBalnce, 10)
-                })
-                window.location.replace('/')
-            } catch (err) {
-                console.error('error occurred in pollWeb3', err)
-                throw err
-            }
-        } else {
-            let web3Copy = state.web3
-            web3Copy.web3Instance = () => web3
-            web3Copy.networkID = await web3.eth.net.getNetworkType()
-            web3Copy.coinbase = (await web3.eth.getAccounts())[0]
-            web3Copy.balance =  await web3.eth.getBalance(state.web3.coinbase)
-            state.web3 = web3Copy
+      const coinbase = (await web3.eth.getAccounts())[0]
+      state.isIntervalExist = true
+
+      // 1. wallet 정보가 없을 때
+      if (typeof coinbase === 'undefined') {
+        clearWeb3Instance(state)
+        // console.log(state)
+
+      // 2. wallet account(coinbase)가 변경됐을 때
+      } else if (state.web3.web3Instance && coinbase !== state.web3.coinbase) {
+        try {
+          const newCoinbase = coinbase
+          const newBalance = await web3.eth.getBalance(newCoinbase)
+          changeCoinbase(state, {
+            coinbase: newCoinbase,
+            balance: Number(newBalance)
+          })
+
+          // 아래 refresh logic 꼭 있어야하는 것은 아님
+          window.location.replace('/')
+        } catch (err) {
+          console.error('error occurred in pollWeb3', err)
+          throw err
         }
+      // 3. 유저 wallet 정보가 존재할 때
+      } else {
+        // console.log(state)
+        state.web3 = {
+          web3Instance: () => web3,
+          networkId: await web3.eth.net.getNetworkType(),
+          balance: await web3.eth.getBalance(coinbase),
+          coinbase,
+        }
+      }
     }, 2000)
+  }
 }
 
+
 const changeCoinbase = (state, payload) => {
-    state.web3.coinbase = payload.coinbase
-    state.web3.balance = parseInt(payload.balance, 10)
+  state.web3.coinbase = payload.coinbase
+  state.web3.balance = parseInt(payload.balance, 10)
 }
 
 const clearWeb3Instance = state => {
-    state.web3.web3Instance = null
-    state.web3.networkID = null
-    state.web3.coinbase = null
-    state.web3.balance = null
+  state.web3.web3Instance = null
+  state.web3.networkID = null
+  state.web3.coinbase = null
+  state.web3.balance = null
 }
